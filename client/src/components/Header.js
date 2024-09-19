9aeceb62507bec4a8fe8c9d6ee8d5a1124aa34ae
baran61 <azak.baran99@gmail.com>
2024-09-19 14:40:40 +0300

    #

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,14 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import './styles.css';
 
 function Header() {
-  const navigate = useNavigate(); // useNavigate kancasını kullandım
+  const navigate = useNavigate(); // useNavigate hook'u kullanıldı
 
   const handleFlightClick = () => {
-    navigate('/my-flights'); // Uçuşlarım sayfasına yönlendirir.
+    navigate('/my-flights'); // Uçuşlarım sayfasına yönlendirir
   };
 
   const handleLogoClick = () => {
-    navigate('/'); // Ana sayfaya yönlendirir.
+    navigate('/'); // Ana sayfaya yönlendirir
   };
 
   return (
@@ -22,21 +22,35 @@ function Header() {
             alt="airplane-mode-on"
             className="logo-icon"
           />
-          <h3>PLANESCAPE</h3>
+          <h3 className="logo-text">PLANESCAPE</h3>
         </div>
         <div className="header-icons">
-          <img  
-            src="https://img.icons8.com/ios-filled/50/7950F2/price-tag.png" 
-            alt="price-tag"
-            className="icon"
-          />
-          <h4>Deals</h4>
-          <img 
-            src="https://img.icons8.com/glyph-neue/64/7950F2/gender-neutral-user.png" 
-            alt="gender-neutral-user"
-            className="icon"
-          />
-          <h4>Joane Smith</h4>
+          <div className="header-item">
+            <h4 className="header-text">Deals</h4>
+            <img  
+              src="https://img.icons8.com/ios-filled/50/7950F2/price-tag.png" 
+              alt="price-tag"
+              className="icon"
+            />
+          </div>
+          <div className="header-item">
+            <h4 className="header-text">Discover</h4>
+            <img 
+              src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/7950F2/external-globe-basic-ui-elements-flatart-icons-outline-flatarticons.png" 
+              alt="discover"
+              className="icon"
+            />
+          </div>
+          <div className="header-item">
+            <h4 className="header-text">Joane Smith</h4>
+            <img 
+              src="https://img.icons8.com/glyph-neue/64/7950F2/gender-neutral-user.png" 
+              alt="gender-neutral-user"
+              className="icon"
+            />
+          </div>
+        </div>
+        <div className="flights-button-container">
           <button onClick={handleFlightClick} className="flights-button">Uçuşlarım</button>
         </div>
       </header>
